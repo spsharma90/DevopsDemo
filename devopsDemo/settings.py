aaff875f8ee66bf37a7a2d94540f760aef8d86e1
Shubham <Shubhamse90@gmail.com>
2025-04-22 12:19:26 +0530

    5th Commit-Environment Variables

--- devopsDemo/settings.py
+++ devopsDemo/settings.py
@@ -20,11 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q34)1*$s$(*$vn&_edyheoh5%vc!ul#&nb6d*xpcw3o!+___=f'
+# SECRET_KEY = 'django-insecure-q34)1*$s$(*$vn&_edyheoh5%vc!ul#&nb6d*xpcw3o!+___=f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
+SECRET_KEY = os.environ.get("SECRET_KEY", "insecure-dev-key")
+DEBUG = os.environ.get("DEBUG", "") != "False"
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class BasicTest(TestCase):
+    def test_home_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
